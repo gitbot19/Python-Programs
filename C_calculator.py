b3053972b9920e20740e90c4b78b463867dff39b
gitbot19 <gitbot19@nineteen.com>
2021-06-18 10:44:33 +0100

    Simple calculator

---
+++ C_calculator.py
@@ -0,0 +1,7 @@
+#Calculator to add two numbers
+number1 = input('Enter a number: ')
+number2 = input('Enter another number: ')
+
+result = float(number1) + float(number2)
+
+print('Result:', result)
